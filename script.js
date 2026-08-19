994a991f8741bb91d8a571ac4943bc7e2876ed59
dbvinculoerotina <dbvinculoerotina@gmail.com>
2026-08-18 21:19:50 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', function () {
+  var toggle = document.querySelector('.nav-toggle');
+  var links = document.querySelector('.nav-links');
+  if (toggle && links) {
+    toggle.addEventListener('click', function () {
+      toggle.classList.toggle('open');
+      links.classList.toggle('open');
+    });
+    links.querySelectorAll('a').forEach(function (a) {
+      a.addEventListener('click', function () {
+        toggle.classList.remove('open');
+        links.classList.remove('open');
+      });
+    });
+  }
+});
